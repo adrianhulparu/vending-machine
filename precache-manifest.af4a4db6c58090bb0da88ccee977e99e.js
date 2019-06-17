2ea33b8915906e690a90120e0482dd6c7b35d506
Adrian <adrianhulparu@gmail.com>
2019-06-17 15:32:40 +0300

    Updates

---
+++ precache-manifest.af4a4db6c58090bb0da88ccee977e99e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6be7b2095715b62ad0c5a42c9d0255cb",
+    "url": "/vending-machine/index.html"
+  },
+  {
+    "revision": "61722e40bf3e6bb0cad9",
+    "url": "/vending-machine/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "03200206f0209464b982",
+    "url": "/vending-machine/static/js/2.acb28f3a.chunk.js"
+  },
+  {
+    "revision": "61722e40bf3e6bb0cad9",
+    "url": "/vending-machine/static/js/main.f6a24e9b.chunk.js"
+  },
+  {
+    "revision": "6970cc35a241b64e454a",
+    "url": "/vending-machine/static/js/runtime~main.7072e60e.js"
+  }
+]);
