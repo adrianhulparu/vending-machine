c98dc083050c1dcd822b9dbd93f19bb8b1485142
Adrian <adrianhulparu@gmail.com>
2019-06-17 17:30:16 +0300

    fix gh-pages routing issue

--- src/store.js
+++ src/store.js
@@ -137,7 +137,7 @@ const initialState = {
   }
 };
 
-export const history = createBrowserHistory();
+export const history = createBrowserHistory({ basename: process.env.PUBLIC_URL });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
